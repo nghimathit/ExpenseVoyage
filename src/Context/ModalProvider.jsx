c4122a95916c514c91d10446ee8642825f42343a
nghimathit <vannghinguyen152001@gmail.com>
2024-09-18 22:54:26 +0700

    Create ModalProvider.jsx

---
+++ src/Context/ModalProvider.jsx
@@ -0,0 +1,32 @@
+
+import Login from '@compoents/Login';
+import React, { createContext, useEffect, useState } from 'react'
+
+export const ModalContext = createContext();
+
+const ModalProvider = ({ children }) => {
+    const [isShow, setisShow] = useState(false);
+    const [content, setContent] = useState();
+    //   không cho cuộn trang web 
+    useEffect(() => {
+        if (isShow) {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = "scroll"
+        }
+
+    }, [isShow]);
+    return (
+        <ModalContext.Provider value={{ setisShow, setContent }}>
+            {children}
+            {isShow && <div className='fixed inset-0 z-10'>
+                <div className='absolute bg-slate-600/60 inset-0 flex items-center justify-center' onClick={() => setisShow(false)}>
+                    <Login />
+                </div>
+
+            </div>}
+        </ModalContext.Provider>
+    )
+}
+
+export default ModalProvider
