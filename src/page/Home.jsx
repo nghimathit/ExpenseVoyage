466d37a170a44c587abd197c2cfb9a4fe7ccdc4b
nghimathit <vannghinguyen152001@gmail.com>
2024-09-18 20:52:04 +0700

    Update Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,13 @@
-import Itinerary from "@compoents/Itinerary";
+
 
 import HomeBanner from "@compoents/Home";
+import Itinerary from "@compoents/Home/Itinerary";
 
 function Home() {
   return (
     <div className="container">
-     <HomeBanner />
-      <Itinerary/>
+      <HomeBanner />
+      <Itinerary />
     </div>
   );
 }
