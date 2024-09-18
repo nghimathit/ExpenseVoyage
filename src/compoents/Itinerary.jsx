533d2a0328fb404fa600616345289269235f0ce0
nghimathit <vannghinguyen152001@gmail.com>
2024-09-18 13:53:15 +0700

    Create Itinerary.jsx

---
+++ src/compoents/Itinerary.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Itinerary = () => {
+    return (
+        <div className='w-full absolute'>
+            <div className='w-1/2 relative'>
+                <img className='w-full h-96' src='https://wanderlog.com/assets/LandingPageProductAnimation__background.jpg'></img>
+                <div className='absolute top-1'>
+                    <img src='https://wanderlog.com/assets/LandingPageProductAnimation__card1.jpg'></img>
+                    <img src='https://wanderlog.com/assets/LandingPageProductAnimation__card2.jpg'></img>
+                    <img src='https://wanderlog.com/assets/LandingPageProductAnimation__card3.jpg'></img>
+
+
+
+
+                </div>
+            </div>
+            <div className='w-1/2'></div>
+        </div>
+    )
+}
+
+export default Itinerary
