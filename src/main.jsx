2df3b9a9efcee204c6fd9bb813d6bb9ed18742e5
nghimathit <vannghinguyen152001@gmail.com>
2024-09-18 22:54:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './page/Home.jsx'
 import RootLayout from './page/RootLayout'
+import ModalProvider from '@Context/ModalProvider'
 
 
 
@@ -21,9 +22,9 @@ const router = createBrowserRouter([
   }
 ])
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <RouterProvider router={router} />
+  <ModalProvider >
+    <RouterProvider router={router} />
+  </ModalProvider>
 
 
-  //</StrictMode>,
 )
