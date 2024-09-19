702a7b09442595afcc4903593f355996f11aa614
IYC054 <trieunhananh@gmail.com>
2024-09-19 12:36:07 +0700

    a

--- src/compoents/Footer.jsx
+++ src/compoents/Footer.jsx
@@ -8,35 +8,45 @@ const Footer = () => {
                 <a href="#"  className="ed-title"> every destination</a>
             </h3>
 
-            <div className="text-muted"></div>
-            <div className="col-sm-6 col-md-3">
-                <div className="col-md-4 mt-3">
-                    <h3 className="footer-title font-weight-bold mb-3 line-height-1">
-                        The best 
-                        <a href="#" className="ed-title"> road trip </a>
-                        planner
-                    </h3>
-                    <div className="text-muted">
-                    Use Expense Voyage as a route map showing directions, distances, and driving times between different attractions you might want to visit.
+            <footer className='footer'>
+                <div className='container'>
+                    <div className='Footer'>
+                        <div class="d-none d-sm-flex row">
+                            <div className='col-sm-3'>
+                                Made With ❤ in SF & more<br></br>
+                                © 2024 Travelchime Inc.
+                            </div>
+                            <div className='col-sm-4'>
+                                <div className='font-bold mb-2'>Wandaerlog</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                            </div>
+                            <div className='col-sm-3'>
+                            <div className='font-bold mb-2'>Wandaerlog</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                                <div className='mb-2'>a</div>
+                            </div>
+                            <div className='col-sm-2'>
+                            <div className='font-bold mb-2'>Get the App</div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-4 mt-3">
-                    <h3 className="footer-title font-weight-bold mb-3 line-height-1">
-                    The best vacation planner
-                    </h3>
-                    <div className="text-muted">
-                    Use Expense Voyage to map your journey to figure out the best routes, keep track of hotel and flight bookings and reservations, and read guides from other trip planning websites.
-                    </div>
-                </div>
-                <div className="col-md-4 mt-3">
-                    <h3 className="footer-title font-weight-bold mb-3 line-height-1">
-                    The best group itinerary planner
-                    </h3>
-                    <div className="text-muted">
-                    Use Expense Voyage to share your itinerary with tripmates, friends, and families and collaborate in real time, so everyone stays in the loop.
-                    </div>
-                </div>
-            </div>
+            </footer>
+           
         </div>
     )
 }
