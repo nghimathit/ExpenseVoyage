ea32c696820961f706d634c4d98fb2bd643d9c9e
jortboy2 <vonghung849@gmail.com>
2024-09-19 08:43:48 +0700

    fix

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import HomeBanner from "@compoents/HomeBanner";
-=======
 
 
-import HomeBanner from "@compoents/Home";
 import Itinerary from "@compoents/Home/Itinerary";
->>>>>>> b795e93689fdf3a97a8e3cb93bdaf42994b5198e
 
 function Home() {
   return (
